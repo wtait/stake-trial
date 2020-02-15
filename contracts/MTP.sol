868fba03ad10ca7e33b1e1e3854fc556ac97199a
Wade <wade.tait@gmail.com>
2020-02-15 13:41:55 -0600

    adds mtp contract outline

--- contracts/MTP.sol
+++ contracts/MTP.sol
@@ -1,1 +1,42 @@
 pragma solidity 0.5.12;
+
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC20/ERC20.sol";
+import "../node_modules/openzeppelin-solidity/contracts/token/ERC721/IERC721.sol";
+
+// Pragma statements
+// Import statements
+// Interfaces
+// Libraries
+// Contracts
+
+contract MTP {
+
+// Type declarations
+// State variables
+// Events
+// Functions
+
+    constructor() public {
+        // ...
+    }
+
+
+    // External functions
+    // ...
+
+    // External functions that are view
+    // ...
+
+    // External functions that are pure
+    // ...
+
+    // Public functions
+    // ...
+
+    // Internal functions
+    // ...
+
+    // Private functions
+    // ...
+}
+
